cee5b0aecb9ecdccfaee9c73af74f1894ba740cb
Hugo Rebelo <hugo@hugorebelo.com>
2015-03-30 18:50:44 -0300

    adding files

---
+++ README.md
@@ -0,0 +1,47 @@
+# Cordova a7printer Plugin
+
+Bluetooth communication with a7printer
+
+## Using
+Clone the plugin
+
+    $ git clone https://github.com/indigoway/cordova-plugin-a7printer.git
+
+Create a new Cordova Project
+
+    $ cordova create a7printer com.example.a7printerapp A7printer
+    
+Install the plugin
+
+    $ cd a7printer
+    $ cordova plugin install ../cordova-plugin-a7printer
+    
+
+Edit `www/js/index.js` and add the following code inside `onDeviceReady`
+
+```js
+    var success = function(message) {
+        alert(message);
+    }
+
+    var failure = function() {
+        alert("Error calling A7printer Plugin");
+    }
+
+    hello.greet("World", success, failure);
+```
+
+Install iOS or Android platform
+
+    cordova platform add ios
+    cordova platform add android
+    
+Run the code
+
+    cordova run 
+
+## More Info
+
+For more information on setting up Cordova see [the documentation](http://cordova.apache.org/docs/en/4.0.0/guide_cli_index.md.html#The%20Command-Line%20Interface)
+
+For more info on plugins see the [Plugin Development Guide](http://cordova.apache.org/docs/en/4.0.0/guide_hybrid_plugins_index.md.html#Plugin%20Development%20Guide)
---
+++ src/android/A7printer.java
@@ -0,0 +1,135 @@
+package com.example.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import inputservice.NfePrinter.BoletoPrinter;
+import inputservice.NfePrinter.BoletoUtils;
+import inputservice.NfePrinter.NfePrinterA7;
+import inputservice.NfePrinter.ReceiptPrinterA7;
+import android.os.Handler;
+import java.io.UnsupportedEncodingException;
+import android.widget.Toast;
+
+public class Hello extends CordovaPlugin {
+
+    /* Adicionado  */
+    public static NfePrinterA7 nfeprinter;
+    public static BoletoPrinter boletoprinter;
+    public static ReceiptPrinterA7 receiptprinter;
+    private static BoletoUtils boleto;
+    private static int btntype = 0;
+    private static boolean connected = false;
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("greet")) {
+
+            nfeprinter = new NfePrinterA7();
+            boletoprinter = new BoletoPrinter();
+            receiptprinter = new ReceiptPrinterA7();
+
+            genBoletoByClassA7();
+
+            String name = data.getString(0);
+            String message = "Hello, " + name;
+            callbackContext.success(message);
+
+            return true;
+
+        } else {
+            
+            return false;
+
+        }
+    }
+
+    public void genBoletoByClassA7(){
+        connected = boletoprinter.connect(false);
+        if(connected){
+
+            boleto = new BoletoUtils();
+            boleto.setLinhaDigitavel("23792.38401 61130.370598 68001.271805 1 56220000320000");
+            boleto.setNomeBanco("Bradesco");
+            boleto.setCodBanco("237-2");
+            boleto.setLocalPagamento("Banco Bradesco S.A.");
+            boleto.setLocalOpcionalPagamento("Pagavel preferencialmente na rede bradesco ou banco postal.");
+            boleto.setVencimento("27/02/2013");
+            boleto.setCedente("Input Service Informatica Ltda");
+            boleto.setAgenciaCodigoCedente("02384-1 / 0012718-3");
+            boleto.setDatadocumento("06/02/2013");
+            boleto.setNumeroDocumento("00000TESTE");
+            boleto.setEspecieDoc("OU");
+            boleto.setAceite("Nao");
+            boleto.setDataProcessameto("06/02/2013");
+            boleto.setNossoNumero("06/11/303705968-5");
+            boleto.setUsoDoBanco("08650");
+            boleto.setCip("000");
+            boleto.setCarteira("06");
+            boleto.setEspecieMoeda("R$");
+            boleto.setQuantidade("");
+            boleto.setValor("");
+            boleto.setValorDocumento("3.200,00");
+            boleto.setInstrucoesCedente(new String[]{"Instrucoes de responsabilidade do cedente    *** Valores expressos em R$ ***","","","","","",""});
+            boleto.setDesconto(" -200,00");
+            boleto.setDeducoes("  -10,00");
+            boleto.setMulta("   50,00");
+            boleto.setAcrescimos("   10,00");
+            boleto.setValorCobrado("3.050,00");
+            boleto.setSacadoNome("INPUT SERVICE INFORMATCA LTDA");
+            boleto.setSacadoEndereco("RUA DEPUTADO MIGUEL PETRELLI, 355");
+            boleto.setSacadoCep("06705-442");
+            boleto.setSacadoCidade("COTIA");
+            boleto.setSacadoUF("SP");
+            boleto.setSacadoCnpj("061.557.856/0001-57");
+
+            // inicio uma outrathread para imprimir o boleto
+            Thread printerThread = new Thread() {
+                // setting the behavior we want from the Thread
+                @Override
+                public void run() {
+                    threadHandlerBoleto.sendEmptyMessage(0);
+                }
+            };
+            printerThread.start();
+
+
+            //passei a imprimir o boleto em uma thread separada pois estava travando a tela
+            /*boletoprinter.getMobilePrinter().Reset();
+            boletoprinter.printBoleto(boleto);*/
+
+            btntype = 1;
+            Thread currentThread = new Thread();
+            currentThread.start();
+
+        }else{
+            //Toast.makeText(this, "Não foi possível realizar a conexão  com a impressora!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    // manages Threads messages of Boleto
+    static private Handler threadHandlerBoleto = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            // handling messages and acting for the Thread goes here
+            boletoprinter.getMobilePrinter().Reset();
+            boletoprinter.printBoleto(boleto);
+        }
+    };
+
+    // manages Threads messages of Receipt
+    static private Handler threadHandlerReceipt = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            // handling messages and acting for the Thread goes here
+            if(msg.what==1){
+               //
+            }else{
+                while (receiptprinter.getMobilePrinter().QueryPrinterStatus()!= 0) {
+                }
+                receiptprinter.disconnect();
+            }
+        }
+    };
+}
