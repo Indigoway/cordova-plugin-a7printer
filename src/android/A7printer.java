4d04a1e1ae1679c6726dd8992ca720f649e27072
Hugo Rebelo <hugo@hugorebelo.com>
2015-04-24 20:07:27 -0300

    Parametrizando boleto

--- src/android/A7printer.java
+++ src/android/A7printer.java
@@ -1,13 +1,17 @@
 package com.indigoway.phonegap.plugin;
 
+import android.util.Log;
+
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import inputservice.NfePrinter.BoletoPrinter;
 import inputservice.NfePrinter.BoletoUtils;
 import inputservice.NfePrinter.NfePrinterA7;
 import inputservice.NfePrinter.ReceiptPrinterA7;
+import java.util.ArrayList;
 import android.os.Handler;
 import java.io.UnsupportedEncodingException;
 import android.widget.Toast;
@@ -30,11 +34,31 @@ public class A7printer extends CordovaPlugin {
     }
 
     @Override
-    public boolean execute(String action, final JSONArray data, final CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
+        System.out.println("*****JARRAY data*****" + data.length());
+
+        JSONObject json_data = null;
+
+        for(int i=0; i<data.length(); i++){
+            json_data = data.getJSONObject(i);
+
+//            Log.i(  "log_tag", "_id" + json_data.getString("account") +
+//                    ", mall_name" + json_data.getInt("_id") +
+//                    ", location" + json_data.getString("number") +
+//                    ", telephone" + json_data.getString("url") +
+//                    ",----" + json_data.getString("balance") +
+//                    ",----" + json_data.getString("credit") +
+//                    ",----" + json_data.getString("displayName")
+//            );
+        }
+
+
         if (action.equals("boleto")) {
+            final JSONObject boletoParams = json_data;
             cordova.getThreadPool().execute(new Runnable() {
+                JSONObject p = boletoParams;
                 public void run() {
-                    genBoletoByClassA7();
+                    genBoletoByClassA7(p);
                     callbackContext.success("Deu certo!"); // Thread-safe.
                 }
             });
@@ -44,46 +68,92 @@ public class A7printer extends CordovaPlugin {
         }
     }
 
-    public void genBoletoByClassA7(){
-        
+    public void genBoletoByClassA7(JSONObject boletoData ){
+
+        System.out.println("*****JARRAY data*****" + boletoData.toString());
+
         boletoprinter = new BoletoPrinter();
 
         connected = boletoprinter.connect(false);
         if(connected){
             boleto = new BoletoUtils();
-            boleto.setLinhaDigitavel("23792.38401 61130.370598 68001.271805 1 56220000320000");
-            boleto.setNomeBanco("Bradesco");
-            boleto.setCodBanco("237-2");
-            boleto.setLocalPagamento("Banco Bradesco S.A.");
-            boleto.setLocalOpcionalPagamento("Pagavel preferencialmente na rede bradesco ou banco postal.");
-            boleto.setVencimento("27/02/2013");
-            boleto.setCedente("Input Service Informatica Ltda");
-            boleto.setAgenciaCodigoCedente("02384-1 / 0012718-3");
-            boleto.setDatadocumento("06/02/2013");
-            boleto.setNumeroDocumento("00000TESTE");
-            boleto.setEspecieDoc("OU");
-            boleto.setAceite("Nao");
-            boleto.setDataProcessameto("06/02/2013");
-            boleto.setNossoNumero("06/11/303705968-5");
-            boleto.setUsoDoBanco("08650");
-            boleto.setCip("000");
-            boleto.setCarteira("06");
-            boleto.setEspecieMoeda("R$");
-            boleto.setQuantidade("");
-            boleto.setValor("");
-            boleto.setValorDocumento("3.200,00");
-            boleto.setInstrucoesCedente(new String[]{"Instrucoes de responsabilidade do cedente    *** Valores expressos em R$ ***","","","","","",""});
-            boleto.setDesconto(" -200,00");
-            boleto.setDeducoes("  -10,00");
-            boleto.setMulta("   50,00");
-            boleto.setAcrescimos("   10,00");
-            boleto.setValorCobrado("3.050,00");
-            boleto.setSacadoNome("INPUT SERVICE INFORMATCA LTDA");
-            boleto.setSacadoEndereco("RUA DEPUTADO MIGUEL PETRELLI, 355");
-            boleto.setSacadoCep("06705-442");
-            boleto.setSacadoCidade("COTIA");
-            boleto.setSacadoUF("SP");
-            boleto.setSacadoCnpj("061.557.856/0001-57");
+
+            if (boletoData.has("linhaDigitavel"))
+                boleto.setLinhaDigitavel(boletoData.optString("linhaDigitavel") );
+            if (boletoData.has("nomeBanco"))
+                boleto.setNomeBanco(boletoData.optString("nomeBanco"));
+            if (boletoData.has("codBanco"))
+                boleto.setCodBanco(boletoData.optString("codBanco"));
+            if (boletoData.has("localPagamento"))
+                boleto.setLocalPagamento(boletoData.optString("localPagamento"));
+            if (boletoData.has("localOpcionalPagamento"))
+                boleto.setLocalOpcionalPagamento(boletoData.optString("localOpcionalPagamento"));
+            if (boletoData.has("vencimento"))
+                boleto.setVencimento(boletoData.optString("vencimento"));
+            if (boletoData.has("cedente"))
+                boleto.setCedente(boletoData.optString("cedente"));
+            if (boletoData.has("agenciaCodigoCedente"))
+                boleto.setAgenciaCodigoCedente(boletoData.optString("agenciaCodigoCedente"));
+            if (boletoData.has("datadocumento"))
+                boleto.setDatadocumento(boletoData.optString("datadocumento"));
+            if (boletoData.has("numeroDocumento"))
+                boleto.setNumeroDocumento(boletoData.optString("numeroDocumento"));
+            if (boletoData.has("especieDoc"))
+                boleto.setEspecieDoc(boletoData.optString("especieDoc"));
+            if (boletoData.has("aceite"))
+                boleto.setAceite(boletoData.optString("aceite"));
+            if (boletoData.has("dataProcessameto"))
+                boleto.setDataProcessameto(boletoData.optString("dataProcessameto"));
+            if (boletoData.has("nossoNumero"))
+                boleto.setNossoNumero(boletoData.optString("nossoNumero"));
+            if (boletoData.has("usoDoBanco"))
+                boleto.setUsoDoBanco(boletoData.optString("usoDoBanco"));
+            if (boletoData.has("cip"))
+                boleto.setCip(boletoData.optString("cip"));
+            if (boletoData.has("carteira"))
+                boleto.setCarteira(boletoData.optString("carteira"));
+            if (boletoData.has("especieMoeda"))
+                boleto.setEspecieMoeda(boletoData.optString("especieMoeda"));
+            if (boletoData.has("quantidade"))
+                boleto.setQuantidade(boletoData.optString("quantidade"));
+            if (boletoData.has("valor"))
+                boleto.setValor(boletoData.optString("valor"));
+            if (boletoData.has("valorDocumento"))
+                boleto.setValorDocumento(boletoData.optString("valorDocumento"));
+            if (boletoData.has("instrucoesCedente"))
+                try {
+                    JSONArray values = boletoData.getJSONArray("instrucoesCedente");
+                    String[] instrucoesCedenteArray = new String[7];
+
+                    for (int i = 0; i < instrucoesCedenteArray.length ; i++)
+                        instrucoesCedenteArray[i] = values.optString(i,"");
+
+                    boleto.setInstrucoesCedente(instrucoesCedenteArray);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            if (boletoData.has("desconto"))
+                boleto.setDesconto(boletoData.optString("desconto"));
+            if (boletoData.has("deducoes"))
+                boleto.setDeducoes(boletoData.optString("deducoes"));
+            if (boletoData.has("multa"))
+                boleto.setMulta(boletoData.optString("multa"));
+            if (boletoData.has("acrescimos"))
+                boleto.setAcrescimos(boletoData.optString("acrescimos"));
+            if (boletoData.has("valorCobrado"))
+                boleto.setValorCobrado(boletoData.optString("valorCobrado"));
+            if (boletoData.has("sacadoNome"))
+                boleto.setSacadoNome(boletoData.optString("sacadoNome"));
+            if (boletoData.has("sacadoEndereco"))
+                boleto.setSacadoEndereco(boletoData.optString("sacadoEndereco"));
+            if (boletoData.has("sacadoCep"))
+                boleto.setSacadoCep(boletoData.optString("sacadoCep"));
+            if (boletoData.has("sacadoCidade"))
+                boleto.setSacadoCidade(boletoData.optString("sacadoCidade"));
+            if (boletoData.has("sacadoUF"))
+                boleto.setSacadoUF(boletoData.optString("sacadoUF"));
+            if (boletoData.has("sacadoCnpj"))
+                boleto.setSacadoCnpj(boletoData.optString("sacadoCnpj"));
 
             boletoprinter.getMobilePrinter().Reset();
             boletoprinter.printBoleto(boleto);
